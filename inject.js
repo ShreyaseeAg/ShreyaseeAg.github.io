111cee835da46e6bf404f51b9c3f75ff4c82e5de
ShreyaseeAg <shreyasee.agarwal@jecrcu.edu.in>
2025-04-18 10:54:45 +0530

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+  fetch('header.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('header-container').innerHTML = data);
+
+  fetch('footer.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('footer-container').innerHTML = data);
+});
